c20a9963706eb65b0551f3996c387bc159f8ba39
Suzen Matabane <100088477+SMatabane@users.noreply.github.com>
2024-02-27 10:05:55 +0200

    Update EditItem.js

--- scoop_smile/src/items/EditItem.js
+++ scoop_smile/src/items/EditItem.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
-export default function EditUser() {
+export default function EditItems() {
   let navigate = useNavigate();
 
   const { id } = useParams();
